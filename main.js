342bf052007598bfd3733fa713fd7630f7165b53
ben <x_z_gfree@163.com>
2023-01-05 21:17:14 +0800

    feature: add background music.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 import "./style.scss";
-
+import { Howl } from "howler";
+const sound = new Howl({
+  src: [
+    "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+  ],
+  loop: true,
+  preload: true,
+});
 window.onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    window.WeixinJSBridge &&
+      window.WeixinJSBridge.invoke(
+        "getNetworkType",
+        {},
+        () => {
+          sound.play();
+        },
+        false
+      );
     clearTimeout(c);
   }, 1000);
 };

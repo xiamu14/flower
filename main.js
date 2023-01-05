29d82e64f44234fe2530bf576a3235ec002bfda2
ben <x_z_gfree@163.com>
2023-01-05 21:29:24 +0800

    feature: enable background music autoplay.

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 import "./style.scss";
-import { Howl } from "howler";
-const sound = new Howl({
-  src: [
-    "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
-  ],
-  loop: true,
-  preload: true,
-});
+
 window.onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
-    window.WeixinJSBridge &&
-      window.WeixinJSBridge.invoke(
-        "getNetworkType",
-        {},
-        () => {
-          sound.play();
-        },
-        false
-      );
+
     clearTimeout(c);
   }, 1000);
 };
